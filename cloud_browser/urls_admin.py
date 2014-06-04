a68960aa1119dc509f19d6ae5eea1a98817c8a91
jthompsonsmi <jthompsonsmi@gmail.com>
2014-06-03 17:49:17 -0700

    Cleaned up commented code and newline.

--- cloud_browser/urls_admin.py
+++ cloud_browser/urls_admin.py
@@ -5,8 +5,6 @@ from django.views.generic.base import RedirectView
 from cloud_browser.app_settings import settings
 
 urlpatterns = patterns('cloud_browser.views',  # pylint: disable=C0103
-    #url(r'^$', redirect_to, name="cloud_browser_index",
-        #kwargs={'url': 'browser'}),
     url(r'^$', RedirectView.as_view(url='browser'), name="cloud_browser_index"), 
     url(r'^browser/(?P<path>.*)$', 'browser', name="cloud_browser_browser",
         kwargs={'template': "cloud_browser/admin/browser.html"}),
